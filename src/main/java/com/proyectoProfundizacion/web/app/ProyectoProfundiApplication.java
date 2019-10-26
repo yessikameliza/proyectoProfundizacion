4a79f5c70368167b9459aef7d09fb2da6c4bde37
unknown <ymvelasquezr@uqvirtual.edu.co>
2019-10-26 11:17:23 -0500

    empezar proyecto

---
+++ src/main/java/com/proyectoProfundizacion/web/app/ProyectoProfundiApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoProfundizacion.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoProfundiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoProfundiApplication.class, args);
+	}
+
+}
